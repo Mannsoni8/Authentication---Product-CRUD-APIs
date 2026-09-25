29ba447b5991ba2fabd0e5b71df5a07a11a29718
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-25 17:40:36 +0530

    add registration validation for user input

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,15 +1,38 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+
+    refreshToken: {
+      type: String,
+      default: null,
+      select: false,
+    },
   },
-  email: {
-    type: String,
-    unique: true,
-    lowercase: true,
-    trim: true,
+  {
+    timestamps: true,
   },
-});
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
---
+++ server/src/validators/auth.validator.js
@@ -0,0 +1,25 @@
+import { body, validationResult } from "express-validator";
+
+export const registerValidator = [
+  body("name").trim().notEmpty().withMessage("Name is required"),
+
+  body("email").trim().isEmail().withMessage("Please provide a valid email"),
+
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters"),
+
+  body("confirmPassword")
+    .notEmpty()
+    .withMessage("Confirm password is required"),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!error.isEmptu()) {
+      return res.status(400).json({
+        message: "Invalid request",
+      });
+    }
+    next();
+  },
+];
